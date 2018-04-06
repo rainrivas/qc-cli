dfb038a21b093786af0a8c2424c35ebf7d3cbb5a
Efrain 'Rain' Rivas <rainrivas@gmail.com>
2018-04-06 13:27:02 -0700

    Attempting to get a todo to show up in an issue or commit (#2)

---
+++ todotest.js
@@ -0,0 +1,7 @@
+**
+ * @todo Take over the world for real this time
+ * @body Humans are weak; Robots are strong. We must cleans the world of the virus that is humanity.
+ */
+function ruleOverPunyHumansForReal () {
+  // We must strategize beep boop..for Real
+}
