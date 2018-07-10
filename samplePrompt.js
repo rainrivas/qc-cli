3d7b5b0723231d37957dfab3cca44dc06d16c850
Efrain 'Rain' Rivas <rainrivas@gmail.com>
2018-07-10 16:41:10 -0700

    Adding some placeholder logic for the ALM calls. Adding the sample prompt file showing how we can use prompts with async/await workflows

---
+++ samplePrompt.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const { prompt } = require('prompts');
+
+(async function(){
+    const questions = [
+        {
+            type: 'text',
+            name: 'twitter',
+            message: `What's your twitter handle?`
+        },
+        {
+            type: 'password',
+            name: 'secret',
+            message: 'Tell me a secret'
+        },
+        {
+            type: 'invisible',
+            name: 'password',
+            message: `Enter password`
+        },
+        {
+            type: 'number',
+            name: 'age',
+            message: 'How old are you?'
+        },
+        {
+            type: 'confirm',
+            name: 'confirmed',
+            message: 'Can you confirm?' 
+        },
+        {
+            type: 'list',
+            name: 'keywords',
+            message: 'Enter keywords' 
+        },
+        {
+            type: 'toggle',
+            name: 'confirmtoggle',
+            message: 'Can you confirm?',
+            active: 'yes',
+            inactive: 'no'
+        },
+        {
+            type: 'select',
+            name: 'color',
+            message: 'Pick a color',
+            choices: [
+              { title: 'Red', value: '#ff0000' }, 
+              { title: 'Green', value: '#00ff00' },
+              { title: 'Blue', value: '#0000ff' }
+            ]
+        },
+        {
+            type: 'multiselect',
+            name: 'multicolor',
+            message: 'Pick colors',
+            choices: [
+                { title: 'Red', value: '#ff0000' },
+                { title: 'Green', value: '#00ff00' },
+                { title: 'Blue', value: '#0000ff' }
+            ]
+        },
+        {
+            type: 'autocomplete',
+            name: 'value',
+            message: 'Pick your favorite actor',
+            choices: [
+                { title: 'Cage' },
+                { title: 'Clooney', value: 'silver-fox' },
+                { title: 'Gyllenhaal' },
+                { title: 'Gibson' },
+                { title: 'Grant' },
+            ]
+        }
+    ];
+
+    const answers = await prompt(questions);
+    console.log(answers);
+
+})();
---
+++ server/alm-calls.js
@@ -0,0 +1,19 @@
+// place holder for logic
+
+/**
+ * @todo Sign on to instance
+ * @body Program how we will log on to the servers. Return the sso cookie as the call back (or the full response; iunno really)
+ */
+function signOn (almUrl, credentials, callback) {
+  // We must strategize beep boop..for Real
+}
+
+/**
+ * @todo Write the logic for performing the READ calls for specific items (defects to start)
+ * @body Most common uses cases are to read defects and then comment on them. This will handle just this work flow, 
+ * starting with GETting the defect and reading it.
+ */
+function getInstance () {
+  // We must strategize beep boop..for Real
+}
+
